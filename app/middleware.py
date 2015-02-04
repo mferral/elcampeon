d925b7dc7aacd301d4ffb524367847e08886efe7
Mikko <mferral@gmail.com>
2015-02-04 13:43:14 -0600

    noticias_principal middle

--- app/middleware.py
+++ app/middleware.py
@@ -5,6 +5,6 @@ class Evalua:
 	def process_view(self, request, view_func, view_args, view_kwargs):
 		print request.path
 		if 'nombre' not in request.session and '/acceso/' not in request.path and '/validar/' not in request.path and request.path!='/' :
-			if request.path!='noticias/' and request.path!='tabla_noticias/' :
+			if request.path!='noticias/' and request.path!='tabla_noticias/' and request.path!='noticias_principal/' :
 				print 'fallo en el middleware'
 				return redirect('/acceso')
